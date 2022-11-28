1a4cf0df7392f0b8c1c8e1c926fc40a86328fa91
Mateus Pereira Rodrigues <86198294+mateusmaiadb@users.noreply.github.com>
2022-11-28 16:51:44 -0300

    subindo projeto

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,7 +1,14 @@
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
 export default function About(){
     return (
         <header>
             <h1>About</h1>
+            <Link to="/">
+            <Button variant="contained">Inicio</Button>
+            </Link>
+
         </header>
     )
 }
